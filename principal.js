b908d8312605499f1757b4674022c5578eef37da
luisperzvz <94600352+luisperzvz@users.noreply.github.com>
2021-12-12 22:11:10 +0100

    Add files via upload

--- principal.js
+++ principal.js
@@ -27,3 +27,25 @@ const elementos=document.getElementosByTagName("Lista");
 const tabla=document.getElementById("Tablero");
 const contador=document.getElementById("Intentos");
 const niveles=document.getElementById("Niveles");
+
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random()*max) -min;
+}
+
+function comprobarVencedor() {
+for (const casilla of elementos) {
+    const ilumniado=casilla.classList.contains("Ilumniados");
+    if (ilumniado=false) {
+        return;
+    }
+}
+alert("¡Eres el vencedor!");
+ganador=true;
+}
+
+const data=new FormData(event.target.value);
+const url=new URl("personalizado.html", windowLocation.origin);
+const luces=data.get("luces");
+const filas=data.get("filas");
+const columnas=data.get("columnas");
+
